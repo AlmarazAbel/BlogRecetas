0622d61d2da4eabc564360a0f137d72e791df2df
Abel Almaraz <almarazabeljesus@gmail.com>
2026-08-17 02:45:55 -0300

    agregamos middlewares validarUsuarios

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { registrarUsuario } from '../controllers/usuario.controller.js';
+import validarUsuario from '../middlewares/validarUsuario.js';
 
 const router = express.Router();
 
 router
-.post('/registro', registrarUsuario);
+.post('/registro', validarUsuario,registrarUsuario);
 
 export default router;
